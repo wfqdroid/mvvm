d71ff720210b6568fd9665a13e51266df49da721
wfq <946586142@qq.com>
2019-06-11 16:19:31 +0800

    init project

---
+++ app/src/main/java/www/imguiderx/mvvmapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package www.imguiderx.mvvmapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
